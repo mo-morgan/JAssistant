4b2c1a4f18e25b75f34c72bfbaebf659b2ccc8ce
Morgan Mo <mmo@norsat.com>
2019-11-05 17:47:13 -0800

    first commit

---
+++ JAssistant/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace JAssistant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonHome = new System.Windows.Forms.Button();
+            this.buttonCalendar = new System.Windows.Forms.Button();
+            this.buttonLearn = new System.Windows.Forms.Button();
+            this.buttonRandomFun = new System.Windows.Forms.Button();
+            this.buttonSettings = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonHome);
+            this.panel1.Controls.Add(this.buttonCalendar);
+            this.panel1.Controls.Add(this.buttonLearn);
+            this.panel1.Controls.Add(this.buttonRandomFun);
+            this.panel1.Controls.Add(this.buttonSettings);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(182, 724);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonHome
+            // 
+            this.buttonHome.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.buttonHome.FlatAppearance.BorderSize = 0;
+            this.buttonHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonHome.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonHome.ForeColor = System.Drawing.Color.White;
+            this.buttonHome.Image = ((System.Drawing.Image)(resources.GetObject("buttonHome.Image")));
+            this.buttonHome.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonHome.Location = new System.Drawing.Point(0, 93);
+            this.buttonHome.Name = "buttonHome";
+            this.buttonHome.Size = new System.Drawing.Size(182, 97);
+            this.buttonHome.TabIndex = 5;
+            this.buttonHome.Text = "Home";
+            this.buttonHome.UseVisualStyleBackColor = true;
+            this.buttonHome.Click += new System.EventHandler(this.buttonHome_Click);
+            // 
+            // buttonCalendar
+            // 
+            this.buttonCalendar.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.buttonCalendar.FlatAppearance.BorderSize = 0;
+            this.buttonCalendar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCalendar.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCalendar.ForeColor = System.Drawing.Color.White;
+            this.buttonCalendar.Image = ((System.Drawing.Image)(resources.GetObject("buttonCalendar.Image")));
+            this.buttonCalendar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonCalendar.Location = new System.Drawing.Point(0, 190);
+            this.buttonCalendar.Name = "buttonCalendar";
+            this.buttonCalendar.Size = new System.Drawing.Size(182, 97);
+            this.buttonCalendar.TabIndex = 4;
+            this.buttonCalendar.Text = "Calendar";
+            this.buttonCalendar.UseVisualStyleBackColor = true;
+            this.buttonCalendar.Click += new System.EventHandler(this.buttonCalendar_Click);
+            // 
+            // buttonLearn
+            // 
+            this.buttonLearn.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.buttonLearn.FlatAppearance.BorderSize = 0;
+            this.buttonLearn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLearn.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLearn.ForeColor = System.Drawing.Color.White;
+            this.buttonLearn.Image = ((System.Drawing.Image)(resources.GetObject("buttonLearn.Image")));
+            this.buttonLearn.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonLearn.Location = new System.Drawing.Point(0, 287);
+            this.buttonLearn.Name = "buttonLearn";
+            this.buttonLearn.Size = new System.Drawing.Size(182, 97);
+            this.buttonLearn.TabIndex = 3;
+            this.buttonLearn.Text = "Learn";
+            this.buttonLearn.UseVisualStyleBackColor = true;
+            this.buttonLearn.Click += new System.EventHandler(this.buttonLearn_Click);
+            // 
+            // buttonRandomFun
+            // 
+            this.buttonRandomFun.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.buttonRandomFun.FlatAppearance.BorderSize = 0;
+            this.buttonRandomFun.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRandomFun.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRandomFun.ForeColor = System.Drawing.Color.White;
+            this.buttonRandomFun.Image = ((System.Drawing.Image)(resources.GetObject("buttonRandomFun.Image")));
+            this.buttonRandomFun.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonRandomFun.Location = new System.Drawing.Point(0, 384);
+            this.buttonRandomFun.Name = "buttonRandomFun";
+            this.buttonRandomFun.Size = new System.Drawing.Size(182, 97);
+            this.buttonRandomFun.TabIndex = 2;
+            this.buttonRandomFun.Text = "Random Fun";
+            this.buttonRandomFun.UseVisualStyleBackColor = true;
+            this.buttonRandomFun.Click += new System.EventHandler(this.buttonRandomFun_Click);
+            // 
+            // buttonSettings
+            // 
+            this.buttonSettings.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.buttonSettings.FlatAppearance.BorderSize = 0;
+            this.buttonSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSettings.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSettings.ForeColor = System.Drawing.Color.White;
+            this.buttonSettings.Image = ((System.Drawing.Image)(resources.GetObject("buttonSettings.Image")));
+            this.buttonSettings.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonSettings.Location = new System.Drawing.Point(0, 481);
+            this.buttonSettings.Name = "buttonSettings";
+            this.buttonSettings.Size = new System.Drawing.Size(182, 97);
+            this.buttonSettings.TabIndex = 1;
+            this.buttonSettings.Text = "Settings";
+            this.buttonSettings.UseVisualStyleBackColor = true;
+            this.buttonSettings.Click += new System.EventHandler(this.buttonSetting_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(182, 87);
+            this.panel3.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 27.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "JAssistant";
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(182, 505);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(835, 219);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(182, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(835, 505);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.panelLeft.Location = new System.Drawing.Point(182, 93);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(14, 91);
+            this.panelLeft.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(1017, 724);
+            this.Controls.Add(this.panelLeft);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(138)))));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSettings;
+        private System.Windows.Forms.Button buttonHome;
+        private System.Windows.Forms.Button buttonCalendar;
+        private System.Windows.Forms.Button buttonLearn;
+        private System.Windows.Forms.Button buttonRandomFun;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panelLeft;
+    }
+}
+
---
+++ JAssistant/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JAssistant
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            panelLeft.Height = buttonHome.Height;
+            panelLeft.Top = buttonHome.Top;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSetting_Click(object sender, EventArgs e)
+        {
+            panelLeft.Height = buttonSettings.Height;
+            panelLeft.Top = buttonSettings.Top;
+        }
+
+        private void buttonHome_Click(object sender, EventArgs e)
+        {
+            panelLeft.Height = buttonHome.Height;
+            panelLeft.Top = buttonHome.Top;
+        }
+
+        private void buttonCalendar_Click(object sender, EventArgs e)
+        {
+            panelLeft.Height = buttonCalendar.Height;
+            panelLeft.Top = buttonCalendar.Top;
+        }
+
+        private void buttonLearn_Click(object sender, EventArgs e)
+        {
+            panelLeft.Height = buttonLearn.Height;
+            panelLeft.Top = buttonLearn.Top;
+        }
+
+        private void buttonRandomFun_Click(object sender, EventArgs e)
+        {
+            panelLeft.Height = buttonRandomFun.Height;
+            panelLeft.Top = buttonRandomFun.Top;
+        }
+    }
+}
